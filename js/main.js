d772b506bfb9c423b4590a473d03f2876fca9d5a
Marcos Zillig <marcos.zillig@gmail.com>
2020-03-22 23:15:57 -0300

    Adiciona assets, css, e js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const members = [
+  { name: "João Pereira", age: 30, role: "Desenvolvedor" },
+  { name: "Diogo Fraga", age: 30, role: "Gestor de Inovaçãp Líder técnico" },
+  { name: "Bia Ferreira", age: 28, role: "Designer de Interface" },
+  { name: "Michael Bravo", age: 34, role: "Desenvolvedor Backend" },
+  { name: "Branca Silva", age: 33, role: "Analista de Qualidade" },
+  { name: "Augusto Costa", age: 24, role: "Desenvolvedor Frontend" },
+  { name: "Marco Dias", age: 41, role: "Desenvolvedor Backend" },
+  { name: "Miguel Silva", age: 32, role: "Desenvolvedor Backend" },
+  { name: "Carlos Gomes", age: 46, role: "Desenvolvedor Fullstack" },
+  { name: "Cristiano Souza", age: 21, role: "Analista de Qualidade" },
+  { name: "Bruno Lima", age: 36, role: "Designer de Interface" }
+];
+
+
